bc6d003c7add531a228e6d4b3142af3b6da493d0
nerdymj <mehulj.work@gmail.com>
2025-12-27 17:02:23 +0530

    Mod2 - Assignment1 tasks py files
    Mod2 - Assignment1 tasks py files

---
+++ Task1.py
@@ -0,0 +1,16 @@
+"""
+Task 1: Perform Basic Mathematical Operations
+"""
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
